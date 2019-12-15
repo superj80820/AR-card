0ba6e8ab3a633281f0f6c097a281d658fafd01f1
superj80820 <superj80820@gmail.com>
2019-12-16 02:39:18 +0800

    test

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const path = require('path')
+const app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.get('/', function(request, response) {
-    response.send('Hello World!')
-})
+app.get('/', (req, res) => res.render('index'))
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
